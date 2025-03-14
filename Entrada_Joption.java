3c19630557f7caf711624f1f06e82136de858e31
Saporit0 <matheussapo@yahoo.com.br>
2025-03-14 11:45:47 -0300

    Commit

---
+++ Entrada_Joption.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Entrada_Joption {
+
+    public static void main(String[] args){
+
+    String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+    String idade = JOptionPane.showInputDialog("Digite sua idade: "); 
+    double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura"));
+
+    
+    int idadeStr = Integer.parseInt(idade);
+
+    JOptionPane.showMessageDialog(null, "Olá, " + nome + "! Você tem " + idadeStr + " anos e " + altura + " de altura.");
+    }
+    
+}
